f592c034ac84a0b4c2c094ccc2b8537ba864ad32
Amine Hajali <belhajali.amine@gmail.com>
2022-11-27 21:50:33 +0100

    Fix network host and port

--- truffle-config.js
+++ truffle-config.js
@@ -7,8 +7,8 @@ const privateKeys = process.env.PRIVATE_KEYS || ""
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
+      host: "ganache",
+      port: 8545,
       network_id: "*" // Match any network id
     },
     ropsten: {
